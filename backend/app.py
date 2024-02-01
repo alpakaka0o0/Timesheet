0718bf9abf5519f0e6978493a9f2b6ee16d108ef
Parkjiwon <pich7755@naver.com>
2024-02-01 15:38:29 -0800

    to yiyoung

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,30 @@
 from flask import Flask
 from models import db, Employee, Pay, Timesheet, Image, Timecheck
+from resources import payroll_ns, upload_ns, modi_ns
+from flask_restx import Api, Resource, Namespace
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_pyfile('config.py')
 db.init_app(app)
+
+api = Api(app, version='1.0', title='Timesheet API', description='Timesheet API', doc='/api-docs')
+api.add_namespace(payroll_ns)
+api.add_namespace(upload_ns)
+api.add_namespace(modi_ns)
+
 with app.app_context():
     db.create_all()
 
-@app.route('/')
+@app.route('/users')
 def users():
     #users 데이터를 Json 형식으로 반환한다.
     return {"members": [{"id" : 1, "name" : "jihyun"},
                         {"id" : 2, "name" : "jerry"}]}
 
 
+ 
 if __name__ == "__main__":
     app.run(debug = True)
 
--- backend/models.py
+++ backend/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Integer, String, DateTime, Column, ForeignKey, Boolean
+from sqlalchemy import Integer, String, DateTime, Column, ForeignKey, Boolean, Time
 
 db = SQLAlchemy()
+
 
 class Image(db.Model):
     id = Column(Integer, primary_key=True)
@@ -11,36 +12,36 @@ class Image(db.Model):
     created_at = Column(DateTime, default = datetime.utcnow)
     
 class Employee(db.Model):
-    id = Column(Integer, primary_key=True)
-    employee_id = Column(String(12), nullable=False)
-    first_name = Column(String(255), nullable=False)
-    pay_per_hour = Column(Integer, nullable=False)
-    over_time_pay = Column(Integer)
-    created_at = Column(DateTime, default = datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.String(255), unique=True, nullable=False)
+    first_name = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 class Timesheet(db.Model):
-    id = Column(Integer, primary_key=True)
-    employee_id = Column(String(12), ForeignKey(Employee.employee_id),nullable=False)
-    week_starting_date = Column(DateTime, nullable=False)
-    created_at = Column(DateTime, default = datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=False)
+    week_starting_date = db.Column(db.DateTime, nullable=False)
+    pay_per_hour = db.Column(db.Integer, nullable=False)
+    over_time_pay = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 class Timecheck(db.Model):
-    id = Column(Integer, primary_key=True)
-    timesheet_id = Column(Integer, ForeignKey(Timesheet.id),nullable=False)
-    date = Column(DateTime)
-    in_time = Column(DateTime)
-    out_time = Column(DateTime)
-    late_entry = Column(Boolean, nullable=False)
-    early_exit = Column(Boolean, nullable=False)
-    created_at = Column(DateTime, default = datetime.utcnow)
-    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    id = db.Column(db.Integer, primary_key=True)
+    timesheet_id = db.Column(db.Integer, db.ForeignKey('timesheet.id'), nullable=False)
+    date = db.Column(db.DateTime)
+    in_time = db.Column(db.DateTime)
+    out_time = db.Column(db.DateTime)
+    late_entry = db.Column(db.Boolean, nullable=False)
+    early_exit = db.Column(db.Boolean, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
 
 class Pay(db.Model):
     id = Column(Integer, primary_key=True)
     date = Column(DateTime)
-    employee_id = Column(String(12), ForeignKey(Employee.employee_id),nullable=False)
+    employee_id = Column(String(255), ForeignKey(Employee.employee_id),nullable=False)
     total_hour = Column(Integer, nullable=False)
     total_pay= Column(Integer, nullable=False)
     created_at = Column(DateTime, default = datetime.utcnow)
